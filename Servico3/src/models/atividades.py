652c78070ea402257a92afddceb112846c1dd143
Jullya-Nigro <nigrojullya07@gmail.com>
2025-11-05 19:43:40 -0300

    feat(models): Definir tabela Atividade

---
+++ Servico3/src/models/atividades.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Date, Integer, String, Float
+from ..config.base import Base
+from sqlalchemy.orm import relationship
+
+
+class Atividade(Base):
+    __tablename__ = 'atividade'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome_atividade = Column(String(100), nullable=False)
+    descricao = Column(Integer, nullable=False)
+    peso_porcento = Column(Float, nullable=False, default=False)
+    data_entrega = Column(Date, nullable=False)
+    turma_id = Column(Integer, nullable=False) 
+    professor_id = Column(Integer, nullable=False)
+
+    atividades = []
+
+    notas = relationship("Nota", back_populates="atividade", cascade="all, delete-orphan")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nome_atividade ": self.nome_atividade ,
+            "descricao": self.descricao,
+            "peso_porcento": self.peso_porcento,
+            "data_entrega": self.data_entrega.isoformat() if self.data_entrega else None,
+            "turma_id": self.turma_id,
+            "professor_id": self.professor_id,}
